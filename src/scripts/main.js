352f703cf09b1702fac592d5d43a017123f00f02
pavlostasiv <pavlostasiv@gmail.com>
2023-05-07 16:30:19 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,28 @@
 
 const people = require('./lib/people');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+const table = document.querySelector('.dashboard');
+const keys = ['name', 'sex', 'born', 'died'];
 
-// write your code here
+people.forEach(persone => {
+  const row = document.createElement('tr');
+
+  for (const key of keys) {
+    const cell = document.createElement('td');
+
+    cell.textContent = persone[key];
+
+    row.append(cell);
+  }
+
+  const ageElement = document.createElement('td');
+  const centureElement = document.createElement('td');
+
+  ageElement.textContent = persone.died - persone.born;
+  centureElement.textContent = Math.ceil(persone.died / 100);
+
+  row.append(ageElement);
+  row.append(centureElement);
+
+  table.firstElementChild.append(row);
+});
